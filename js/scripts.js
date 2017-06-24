0d08dd8e9e18ec491a4639954c19e2eb3c35ed1a
Tbryant <waterstrok@aol.com>
2017-06-23 22:35:12 -0400

    it will be if else

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,52 @@
+// var divisable = function(i) {
+//
+//   countdown.forEach(function(count) {
+//     var greeting;
+//       if (i % 3)  {
+//         greeting = "ping"
+//       }
+
+//       //  else if {
+//       //  (i % 5 === "pong")
+//       // }
+//       //  else if{
+//       //  (i % 15 === "ping pong"))
+//       // }
+//       //  else
+
+
+
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+
     var countdown = [];
-    var UserInput = $("input#number").val()
-    for(i = 0; i <= userInput; i++) {
+    var userInput = parseInt($("input#number").val());
+    for(i = 1; i <= userInput; i++) {
         countdown.push(i);
     }
-     $(".form-group").text(countdown);
-//This is the sort loop after sorting. it displays the results.
+
     countdown.forEach(function(count) {
-//We are taking the countdown input and making the
-//list inside of java script not html
-    $("#list").append("<li>" + count + "</li>");
-  });
+      // divisable();
+      $("#list").append("<li>" + count + "</li>");
 
-  $("form").hide();
-
-  $("#list").show();
+    });
 
     event.preventDefault();
+    });
   });
- });
 
+
+  // $("form").hide();
+  //
+
+
+
+
+
+
+
+
+// document.getElementById('#list').innerHTML = countdown;
 
  // $(".sentence").text(allCaps);
  // var countdown = [];
