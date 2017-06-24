c71e5d3fd6bbac3049e9a0f235aa06221d6bf24e
Tbryant <waterstrok@aol.com>
2017-06-24 14:23:27 -0400

    it will finaly workgit add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-// var divisable = function(i) {
-//
-  // countdown.forEach(function(count) {
-  //   var divisable = function(countdown) {
-      // if (countdown % 3) && (countdown % 15 !== 0) || (year % 400 === 0)) {
-      //   return true;
-      // } else {
-      //   return false;
-      // }
-  //   };
-
-
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
 
@@ -21,7 +9,6 @@ $(document).ready(function() {
 
     // countdown.forEach(function(count) {
       $.each(countdown,function(index,value){
-          // this tests the value presently being "looked at" to see if it is divisible by 3, then sets the array item to "ping" if it is
           if(value % 15 === 0) {
             countdown[index] = 'Ping Pong';
           }
@@ -32,47 +19,12 @@ $(document).ready(function() {
             countdown[index] = 'ping';
           }
         });
-        // just outputting to console
-        // console.log(countdown);
-      // });
 
-      // if (i % 3)  {
-      //   // countdown.replace(i);
-      //   $(".ping").show();
-      // }
-      // var greeting1 = "ping"
-      // if (i % 3)  {
-      //   countdown.push(greeting1);
-      // }
-
-      $("#list").append("<li>" + countdown  + "</li>");
-
-    // });
-    // if (i % 3)  {
-    //   $(".ping").show();
-    // }
+    countdown.forEach(function(count) {
+      $("#list").append("<li>" + count + "</li>");
+    });
+    
     $("#list").show();
     event.preventDefault();
     });
   });
-
-
-  // $("form").hide();
-  //
-
-
-
-
-
-
-
-
-// document.getElementById('#list').innerHTML = countdown;
-
- // $(".sentence").text(allCaps);
- // var countdown = [];
- // var userInput = parseInt(prompt("enter number"));
- // //     console.log = countdown
- // for(i = 0; i <= userInput; i++) {
- //   countdown.push(i);
- // }
